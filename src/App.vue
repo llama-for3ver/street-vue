dfc3ebc69e1859c6ffd9359108dce2fda70bd24a
llama-for3ver <llamaz@rooik.at>
2024-12-18 15:27:13 +1300

    refactor App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
 import StreetView from './components/StreetView.vue'
+import { ref } from 'vue'
+
+const zoom = ref(4)
+const pano = ref("WlMXvion3Q6Rba7QdmHi5A")
+
+const changeZoom = () => {
+  zoom.value = Number(prompt('Enter new zoom (1-5):', zoom.value.toString()))
+}
+const changePano = () => {
+  pano.value = prompt(`Enter new pano ID. Examples: 
+  WlMXvion3Q6Rba7QdmHi5A - Times Square
+  OoRKqxWe0KxxEWUisDIy7w - Amsterdam`.trim(),
+  pano.value) as string
+}
+
 </script>
 
 <template>
   <div class="street-view-panorama">
-    <StreetView id="sv" panoId="WlMXvion3Q6Rba7QdmHi5A" :zoom="4"/>
+    <button style="position:absolute; top: 20px; left: 2%;" @click="changeZoom">Change Zoom</button>
+    <button style="position:absolute; top: 20px; left: 10%;" @click="changePano">Change Pano</button>
+    <StreetView id="sv" :panoId="pano" :zoom="zoom" />
   </div>
 </template>
 
